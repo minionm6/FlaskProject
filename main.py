db298b0fdf7ee40bee9314dd4a8edda244a67cd5
dmitriy <pozdeev.dmitriy.2005@gmail.com>
2026-02-02 16:41:58 +1200

    основные основы

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
-def hello_world():
-    return 'hello worlddddddddddddddd!'
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
-if __name__ =='__main__':
+@app.route('/p2')
+def greet():
+    return render_template('p2.html')
+
+
+
+
+
+if __name__ == '__main__':
     app.run(debug = True)
